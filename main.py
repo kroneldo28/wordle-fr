e6424fe5ced843f7c0861ff65a6d88d40b7d9143
kroneldo28 <kroneldo28@yahoo.fr>
2022-01-15 18:36:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# The French version of the popular game wordle developed by Josh Wardle
+# The original version can be found here ==> https://www.powerlanguage.co.uk/wordle/
+
+# TODO 1: Make a small a library of 5 words to test the game during developement
+# TODO 2: Make a simple version of the game
+# TODO 3: Make a twist to the game so that the player can choose whether to play 5, more or less words
+# TODO 4: Make it Graphical version
+# TODO 5: Build mini librairies with the same number of letters (make it public)
